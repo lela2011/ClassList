78def76fea440cce6a75f8082fd9324c8e60111c
Leon W <lp.wittich@icloud.com>
2020-10-26 14:15:04 +0100

    Starting on List.

--- app/src/main/java/com/example/cleanlist/MainActivity.java
+++ app/src/main/java/com/example/cleanlist/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.cleanlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.cleanlist.databinding.ActivityMainBinding;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    public List<WeekEntry> Dataset;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     private ActivityMainBinding binding;
 
@@ -17,5 +27,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        Dataset.add(new WeekEntry(LocalDate.of(2020, 10, 26), LocalDate.of(2020, 11, 1), "Benedikt", "Nicolas"));
     }
 }
