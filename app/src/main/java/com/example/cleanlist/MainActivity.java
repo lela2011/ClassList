0ee430f43b646ad7e6606594657ce3719643ea1e
Leon W <lp.wittich@icloud.com>
2020-10-26 14:23:54 +0100

    Added Dataset entries up to week 48.

--- app/src/main/java/com/example/cleanlist/MainActivity.java
+++ app/src/main/java/com/example/cleanlist/MainActivity.java
@@ -29,5 +29,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         Dataset.add(new WeekEntry(LocalDate.of(2020, 10, 26), LocalDate.of(2020, 11, 1), "Benedikt", "Nicolas"));
+        Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 2), LocalDate.of(2020, 11, 8), "Alex", "Manuel"));
+        Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 9), LocalDate.of(2020, 11, 15), "Luca", "Jens"));
+        Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 16), LocalDate.of(2020, 11, 22), "Patrick", "Sascha"));
+        Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 23), LocalDate.of(2020, 11, 29), "Marco", "Janis"));
     }
 }
