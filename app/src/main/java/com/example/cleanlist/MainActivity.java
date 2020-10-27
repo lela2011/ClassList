d3a81b4241737df72fcaed914a3f539b6bf92b70
Jakob Mostert <jakob.mostert@outlook.com>
2020-10-27 07:48:41 +0100

    Complete entries

--- app/src/main/java/com/example/cleanlist/MainActivity.java
+++ app/src/main/java/com/example/cleanlist/MainActivity.java
@@ -33,5 +33,11 @@ public class MainActivity extends AppCompatActivity {
         Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 9), LocalDate.of(2020, 11, 15), "Luca", "Jens"));
         Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 16), LocalDate.of(2020, 11, 22), "Patrick", "Sascha"));
         Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 23), LocalDate.of(2020, 11, 29), "Marco", "Janis"));
+        Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 30), LocalDate.of(2020, 12, 6), "Damian", "Marcel"));
+        Dataset.add(new WeekEntry(LocalDate.of(2020, 12, 7), LocalDate.of(2020, 12, 13), "Dario", "Jara"));
+        Dataset.add(new WeekEntry(LocalDate.of(2020, 12, 14), LocalDate.of(2020, 12, 20), "Simon B.", "Benedikt"));
+        Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 4), LocalDate.of(2021, 1, 10), "Nicolas", "Alex"));
+        Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 11), LocalDate.of(2021, 1, 17), "Manuel", "Luca"));
+        Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 18), LocalDate.of(2021, 1, 14), "Jens", "Patrick"));
     }
 }
--- app/src/main/java/com/example/cleanlist/WeekEntry.java
+++ app/src/main/java/com/example/cleanlist/WeekEntry.java
@@ -17,13 +17,4 @@ public class WeekEntry {
         this.firstName = firstName;
         this.secondName = secondName;
     }
-
-    public List<WeekEntry> Dataset;
-
-    Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 30), LocalDate.of(2020, 12, 6), "Damian", "Marcel"));
-    Dataset.add(new WeekEntry(LocalDate.of(2020, 12, 7), LocalDate.of(2020, 12, 13), "Dario", "Jara"));
-    Dataset.add(new WeekEntry(LocalDate.of(2020, 12, 14), LocalDate.of(2020, 12, 20), "Simon B.", "Benedikt"));
-    Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 4), LocalDate.of(2021, 1, 10), "Nicolas", "Alex"));
-    Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 11), LocalDate.of(2021, 1, 17), "Manuel", "Luca"));
-    Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 18), LocalDate.of(2021, 1, 14), "Jens", "Patrick"));
 }
