c57b53e87dc3cb4962a6108fa24fa856c508055d
Leon W <lp.wittich@icloud.com>
2020-10-27 08:04:13 +0100

    Finished recyclerView

--- app/src/main/java/com/example/cleanlist/MainActivity.java
+++ app/src/main/java/com/example/cleanlist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cleanlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        recyclerView = binding.recyclerView;
 
         Dataset.add(new WeekEntry(LocalDate.of(2020, 10, 26), LocalDate.of(2020, 11, 1), "Benedikt", "Nicolas"));
         Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 2), LocalDate.of(2020, 11, 8), "Alex", "Manuel"));
@@ -39,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
         Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 4), LocalDate.of(2021, 1, 10), "Nicolas", "Alex"));
         Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 11), LocalDate.of(2021, 1, 17), "Manuel", "Luca"));
         Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 18), LocalDate.of(2021, 1, 14), "Jens", "Patrick"));
-        Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 18), LocalDate.of(2021, 1, 14), "Jens", "Patrick"));
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new Adapter(Dataset);
+        recyclerView.setAdapter(adapter);
     }
 }
