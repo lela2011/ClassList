291b2c04d1c725d148dad6eddf10c96841d2790f
Jakob Mostert <jakob.mostert@outlook.com>
2020-10-27 07:46:59 +0100

    Add entries

--- app/src/main/java/com/example/cleanlist/WeekEntry.java
+++ app/src/main/java/com/example/cleanlist/WeekEntry.java
@@ -2,6 +2,7 @@ package com.example.cleanlist;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 public class WeekEntry {
 
@@ -16,4 +17,13 @@ public class WeekEntry {
         this.firstName = firstName;
         this.secondName = secondName;
     }
+
+    public List<WeekEntry> Dataset;
+
+    Dataset.add(new WeekEntry(LocalDate.of(2020, 11, 30), LocalDate.of(2020, 12, 6), "Damian", "Marcel"));
+    Dataset.add(new WeekEntry(LocalDate.of(2020, 12, 7), LocalDate.of(2020, 12, 13), "Dario", "Jara"));
+    Dataset.add(new WeekEntry(LocalDate.of(2020, 12, 14), LocalDate.of(2020, 12, 20), "Simon B.", "Benedikt"));
+    Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 4), LocalDate.of(2021, 1, 10), "Nicolas", "Alex"));
+    Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 11), LocalDate.of(2021, 1, 17), "Manuel", "Luca"));
+    Dataset.add(new WeekEntry(LocalDate.of(2021, 1, 18), LocalDate.of(2021, 1, 14), "Jens", "Patrick"));
 }
